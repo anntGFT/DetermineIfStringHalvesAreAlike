d3d388c542cc9a9436ff324791158662b31bbb3c
annt <annt@gft.com>
2022-12-01 15:49:48 +0000

    Done

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,34 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public boolean halvesAreAlike(String s) {
+
+        String firstHalf = s.substring(0, (s.length() - 1) / 2 + 1).toLowerCase();
+        String secondHalf = s.substring((s.length() -1) / 2 + 1).toLowerCase();
+
+        int counterFirstHalf = 0;
+        int counterSecondHalf = 0;
+
+        counterFirstHalf = getCounterFirstHalf(firstHalf, counterFirstHalf);
+
+        counterSecondHalf = getCounterFirstHalf(secondHalf, counterSecondHalf);
+
+        return counterFirstHalf == counterSecondHalf;
+    }
+
+    private int getCounterFirstHalf(String word, int counter) {
+
+        for (int i = 0; i< word.length(); i++){
+
+            if (word.charAt(i)=='a' || word.charAt(i)=='e' || word.charAt(i)=='i' || word.charAt(i)=='o' || word.charAt(i)=='u'){
+
+                counter ++;
+            }
+        }
+        return counter;
     }
 }
--- src/test/java/com/example/SolutionTest.java
+++ src/test/java/com/example/SolutionTest.java
@@ -1,2 +1,45 @@
-package com.example;public class SolutionTest {
+package com.example;
+
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    String s;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void StringWithOnlyTwoVowels(){
+
+        s = "ae";
+
+        assertTrue(solution.halvesAreAlike(s));
+    }
+
+    @Test
+    public void StringWithOnlyOneVowelAtTheFirstHalf(){
+
+        s = "aprc";
+
+        assertFalse(solution.halvesAreAlike(s));
+    }
+
+    @Test
+    public void StringWithThreeVowelsTheFirstHalfAndThreeAtTheSecond(){
+
+        s = "pgfijaepkuliom";
+
+        assertTrue(solution.halvesAreAlike(s));
+    }
 }
